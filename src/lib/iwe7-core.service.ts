f2580b1b763f7c76d21e5226f2b465b0aa70853f
iwe7 <admin@meepo.com.cn>
2018-06-07 08:30:34 +0800

    update

--- src/lib/iwe7-core.service.ts
+++ src/lib/iwe7-core.service.ts
@@ -1,11 +1,13 @@
 import { Iwe7DomService } from './iwe7-dom.service';
 import { Injectable } from '@angular/core';
+import { Iwe7Platform } from './iwe7-platform';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Iwe7CoreService {
   constructor(
+    public platform: Iwe7Platform,
     public dom: Iwe7DomService
   ) { }
 }
